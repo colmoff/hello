404c30c18329cad7a004deb0313cba53431a56cb
Collin Moffatt <cmoffatt3@huskers.unl.edu>
2020-08-20 04:12:10 +0000

    initial commit

---
+++ hello/hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Collin Moffatt
+ * Date: 2020/08/19
+ * 
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+int main(int argc, char **argv) {
+    
+    printf("Hello world!\n");
+    printf("Computer Science\n");
+    
+    return 0;
+}
---
+++ hola.c
@@ -0,0 +1,15 @@
+/**
+ * Author:  Collin
+ * Date:    Moffatt
+ * 
+ * A simple hello world program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Hello world!\n");
+    
+    return 0;
+}
